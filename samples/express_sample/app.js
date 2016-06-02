ea1c55062c9886c1e4438a0392cf113f85b96e53
Marcis Nimants <marcis.nimants@testdevlab.com>
2016-06-02 15:26:59 +0300

    add working get auth req url

--- samples/express_sample/app.js
+++ samples/express_sample/app.js
@@ -2,9 +2,25 @@ var express = require('express');
 var MiraclClient = require('./node_modules/maas-sdk-nodejs/lib/index'); // TODO: to be changed after pushing to git repo
 var app = express();
 
+app.set('view engine', 'ejs');
+
+
 app.get('/', function (req, res) {
-  res.send(MiraclClient());
+  var miracl = new MiraclClient({
+    clientID: "sflyuimq3wcxk",
+    clientSecret: "50cc-Gr-n71uftgJ3XwiJ0lXyq2Yqhbw2f4AAP_3Q6Q",
+    callbackURL: "http://127.0.0.1:5000/c2id"
+  }, done);
+
+  function done(err, config){
+    res.render('index', { auth_url: miracl.get_authorization_request_url() });
+  }
 });
 
-app.listen(3000);
+app.get('/c2id', function(req, res) {
+  console.log(req.query);
+  res.send("success!");
+});
+
+app.listen(5000);
 module.exports = app;
