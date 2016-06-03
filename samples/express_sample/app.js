532c4e22484b395488cb7944812cf4c7793e0323
Marcis Nimants <marcis.nimants@testdevlab.com>
2016-06-03 14:44:31 +0300

    almost everything works without refresh

--- samples/express_sample/app.js
+++ samples/express_sample/app.js
@@ -1,25 +1,68 @@
 var express = require('express');
+var session = require('express-session');
 var MiraclClient = require('./node_modules/maas-sdk-nodejs/lib/index'); // TODO: to be changed after pushing to git repo
 var app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
 
 app.get('/', function (req, res) {
-  var miracl = new MiraclClient({
+  var session = req.session;
+  var miracl = new MiraclClient({ // TODO: initialize this before each req
     clientID: "sflyuimq3wcxk",
     clientSecret: "50cc-Gr-n71uftgJ3XwiJ0lXyq2Yqhbw2f4AAP_3Q6Q",
     callbackURL: "http://127.0.0.1:5000/c2id"
   }, done);
 
   function done(err, config){
-    res.render('index', { auth_url: miracl.get_authorization_request_url() });
+    if(miracl.isAuthorized(session)){
+      console.log("1");
+      miracl.getEmail(session, function(err, email) {
+        console.log(email);
+        res.render('index', { is_authorized: true, email: session.miraclUserinfo.sub });
+      });
+    }
+
+
+    else res.render('index', { is_authorized: false,
+                          auth_url: miracl.getAuthorizationRequestUrl(session) });
   }
 });
 
 app.get('/c2id', function(req, res) {
-  console.log(req.query);
-  res.send("success!");
+  var session = req.session;
+  var miracl = new MiraclClient({ // TODO: initialize this before each req
+    clientID: "sflyuimq3wcxk",
+    clientSecret: "50cc-Gr-n71uftgJ3XwiJ0lXyq2Yqhbw2f4AAP_3Q6Q",
+    callbackURL: "http://127.0.0.1:5000/c2id"
+  }, done);
+
+  function done(err, config){
+    miracl.validateAuthorization(req.query, session, function (err, accessToken) {
+      if(err) res.send("There was an error");
+      else res.redirect('/');
+    });
+  }
+});
+
+app.get('/refresh', function(req, res) {
+  var session = req.session;
+  var miracl = new MiraclClient({ // TODO: initialize this before each req
+    clientID: "sflyuimq3wcxk",
+    clientSecret: "50cc-Gr-n71uftgJ3XwiJ0lXyq2Yqhbw2f4AAP_3Q6Q",
+    callbackURL: "http://127.0.0.1:5000/c2id"
+  }, done);
+
+  function done(err, config){
+    miracl.clearUserInfo(session);
+    res.redirect('/');
+  }
 });
 
 app.listen(5000);
