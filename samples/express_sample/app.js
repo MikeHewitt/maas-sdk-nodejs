fbe3bc1f4567d507705c11f2674e0ebcc8e02829
Marcis Nimants <marcis.nimants@testdevlab.com>
2016-06-06 17:30:26 +0300

    Add working sample with new mpin qr

--- samples/express_sample/app.js
+++ samples/express_sample/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var session = require('express-session');
-var MiraclClient = require('./node_modules/maas-sdk-nodejs/lib/index'); // TODO: to be changed after pushing to git repo
+var miraclClient = require('./node_modules/maas-sdk-nodejs/lib/index'); // TODO: to be changed after pushing to git repo
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -11,71 +11,40 @@ app.use(session({
   saveUninitialized: true
 }))
 
-
-app.get('/', function (req, res) {
-  var session = req.session;
-  var miracl = new MiraclClient({ // TODO: initialize this before each req
+app.use(function(req, res, next) {
+  req.miracl = new miraclClient({ // TODO: initialize this before each req
     clientID: "sflyuimq3wcxk",
     clientSecret: "50cc-Gr-n71uftgJ3XwiJ0lXyq2Yqhbw2f4AAP_3Q6Q",
     callbackURL: "http://127.0.0.1:5000/c2id"
-  }, done);
-
-  function done(err, config){
-    if(miracl.isAuthorized(session)){
-      console.log("1");
-      miracl.getEmail(session, function(err, email) {
-        console.log(email);
-        res.render('index', { is_authorized: true, email: session.miraclUserinfo.sub });
-      });
-    }
+  }, next);
+});
 
 
-    else res.render('index', { is_authorized: false,
-                          auth_url: miracl.getAuthorizationRequestUrl(session) });
+app.get('/', function (req, res) {
+  if(req.miracl.isAuthorized(req.session)){
+    req.miracl.getEmail(req.session, function(err, email) {
+      res.render('index', { is_authorized: true, email: req.session.miraclUserinfo.sub });
+    });
   }
+  else res.render('index', { is_authorized: false,
+                        auth_url: req.miracl.getAuthorizationRequestUrl(req.session) });
 });
 
 app.get('/c2id', function(req, res) {
-  var session = req.session;
-  var miracl = new MiraclClient({ // TODO: initialize this before each req
-    clientID: "sflyuimq3wcxk",
-    clientSecret: "50cc-Gr-n71uftgJ3XwiJ0lXyq2Yqhbw2f4AAP_3Q6Q",
-    callbackURL: "http://127.0.0.1:5000/c2id"
-  }, done);
-
-  function done(err, config){
-    miracl.validateAuthorization(req.query, session, function (err, accessToken) {
-      if(err) res.send("There was an error");
-      else res.redirect('/');
-    });
-  }
+  req.miracl.validateAuthorization(req.query, req.session, function (err, accessToken) {
+    if(err) res.send("There was an error");
+    else res.redirect('/');
+  });
 });
 
 app.get('/refresh', function(req, res) {
-  var session = req.session;
-  var miracl = new MiraclClient({ // TODO: initialize this before each req
-    clientID: "sflyuimq3wcxk",
-    clientSecret: "50cc-Gr-n71uftgJ3XwiJ0lXyq2Yqhbw2f4AAP_3Q6Q",
-    callbackURL: "http://127.0.0.1:5000/c2id"
-  }, done);
-
-  function done(err, config){
-    miracl.clearUserInfo(session);
-    res.redirect('/');
-  }
+  req.miracl.clearUserInfo(req.session);
+  res.redirect('/');
 });
 
 app.get('/logout', function(req, res){
-  var session = req.session;
-  var miracl = new MiraclClient({ // TODO: initialize this before each req
-    clientID: "sflyuimq3wcxk",
-    clientSecret: "50cc-Gr-n71uftgJ3XwiJ0lXyq2Yqhbw2f4AAP_3Q6Q",
-    callbackURL: "http://127.0.0.1:5000/c2id"
-  }, done);
-  function done(err, config){
-    miracl.clearUserInfo(session, true);
-    res.redirect('/');
-  }
+  req.miracl.clearUserInfo(req.session, true);
+  res.redirect('/');
 });
 
 app.listen(5000);
